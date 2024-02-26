7e0467371557ed26d9336a0637c1af7c8ed108f0
Mizuyamu <146126925+Mizuyamu@users.noreply.github.com>
2024-02-26 12:32:52 -0800

    push changes

---
+++ 3D Game Examples/Assets/KayKit_Prototype_Bits_1.0_EXTRA/Scripts/GameSession.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] private int _coinCount;
+    [SerializeField] private TextMeshProUGUI _countCountText;
+    [SerializeField] private CanvasGroup _CanvasGroup;
+    [SerializeField] private bool _fadeIn, _fadeOut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coinCount = 0;
+        _countCountText.text = "Coins: " + _coinCount.ToString();
+        FadeOutUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_fadeIn)
+        {
+            if(_CanvasGroup.alpha < 1)
+            {
+                _CanvasGroup.alpha += Time.deltaTime;
+                if(_CanvasGroup.alpha >= 1)
+                {
+                    _fadeIn = false;
+                }
+            }
+        }
+
+        if(_fadeOut)
+        {
+            if(_canvaGroup.alpha >= 0)
+            {
+                _canvaGroup.alpha -= Time.deltaTime;
+            }
+        }
+    }
+}
